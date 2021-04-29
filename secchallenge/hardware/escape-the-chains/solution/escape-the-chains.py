28fe6d1b0c1169fdf92ceeb0802aa5871a996627
David Nemeth Csoka <encse@csokavar.hu>
2021-04-29 20:15:16 +0200

    x

--- secchallenge/hardware/escape-the-chains/solution/escape-the-chains.py
+++ secchallenge/hardware/escape-the-chains/solution/escape-the-chains.py
@@ -1,24 +1,65 @@
+import json
+import re
+from http.cookiejar import Cookie
 import requests
-from urllib.parse import quote
+import urllib.parse
 
-baud_rate = 0
-data_bits = 0
-parity_bits = 0
-stop_bits = 0
+url_base = 'https://escape-the-chains.secchallenge.crysys.hu'
 
 
-for baud_rate in [115200]:
-    for data_bits in [8]:
-        for stop_bits in [1]:
-            for parity_bits in [0]:
-                serial= f'{{"baud_rate":{baud_rate},"data_bits":{data_bits},"parity_bits":{parity_bits},"stop_bits":{stop_bits},"wiring":{{"wire-gnd":"devicepin-0","wire-tx":"devicepin-1","wire-rx":"devicepin-2"}}}}'
-                
-                print(quote(serial))
-                cookies = dict(
-                    serial=quote(serial),
-                    term='%7B%22shell%22%3A%7B%7D%2C%22controlling_process%22%3A%22serial%22%7D'
-                )
-                r = requests.post('https://escape-the-chains.secchallenge.crysys.hu/api/terminal', cookies=cookies);
+class Terminal(object):
+    def __init__(self):
+        self.session = requests.session()
 
-                print(f'b {baud_rate}, d {data_bits}, s {stop_bits} p {parity_bits}: {r.text}')
+        assert '/static/index.js' in self.session.get(f'{url_base}/').text
+        assert 'api/terminal' in self.session.get(f'{url_base}/static/index.js').text
+        self.run(None)
+
+    def set_wiring(self, gnd, tx, rx):
+        serial = json.loads(urllib.parse.unquote(self.session.cookies.get('serial')))
+        serial['wiring'] = {
+            "wire-gnd":f'devicepin-{gnd}',
+            "wire-tx":f'devicepin-{tx}',
+            "wire-rx":f'devicepin-{rx}'
+        }
+
+        self.session.cookies.set_cookie(Cookie(
+            version=0, name="serial",
+            value=urllib.parse.quote(json.dumps(serial)),
+            port=None, port_specified=False,
+            domain='escape-the-chains.secchallenge.crysys.hu',
+            domain_specified=True, domain_initial_dot=False,
+            path="/", path_specified=True, secure=False,
+            expires=None, discard=True,
+            comment=None, comment_url=None,
+            rest={"SameSite": 'Strict'}, rfc2109=False))
+
+    def run(self, cmd):
+        print(f'> {cmd}')
+        res =  self.session.post(f'{url_base}/api/terminal', data=cmd).text
+        print(res)
+        return res
+
+
+def solve():
+
+    terminal = Terminal()
+
+    assert 'bin' in terminal.run('ls')
+    assert 'ttycon' in terminal.run('ls bin')
+    assert 'dev' in terminal.run('ls')
+    assert 'ttyUSB0' in terminal.run('ls dev')
+    assert 'baud-rate' in terminal.run('ttycon -help')
+    terminal.set_wiring(0, 1, 2)
+    terminal.run('ttycon -baud-rate 115200 -data-bits 8 -stop-bits 1 /dev/ttyUSB0')
+
+    assert 'factory-reset' in terminal.run('help')
+    t = terminal.run('factory-reset')
+    flag = re.search(r'cd21\{.*\}', t).group()
+    return flag
+
+
+if __name__ == "__main__":
+    print(solve())
+
 
